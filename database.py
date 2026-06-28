369cb7a8d546a175ddb3fdb14746ec079082fe01
gnouhpym2005-commits <gnouhpym2005@gmail.com>
2026-06-29 00:31:04 +0700

    Modify Database class to connect to localhost
    Updated database connection parameters to use 'localhost'.

--- database.py
+++ database.py
@@ -1,28 +1,30 @@
 import pyodbc
 
+
 class Database:
+
     def __init__(self):
 
-        self.server = "DESKTOP-RUT4VE1\\SQLEXPRESS"  #thay bằng tên SQL Server trên máy của người sử dụng
+        self.server = "localhost"
         self.database = "CourseRegistrationSystem"
 
     def connect(self):
 
         try:
+
             conn = pyodbc.connect(
-                "DRIVER={ODBC Driver 17 for SQL Server};"
+
+                f"DRIVER={{ODBC Driver 17 for SQL Server}};"
                 f"SERVER={self.server};"
                 f"DATABASE={self.database};"
                 "Trusted_Connection=yes;"
+
             )
-            print("Database connection established successfully.")
+
             return conn
 
         except Exception as e:
-            print("Failed to connect to the database.")
+
             print(e)
+
             return None
-        
-    def close(self, connection):
-        if connection:
-            connection.close()
